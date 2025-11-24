6f6845f797e8c42f3965da8b2455971d58f86dd5
ayushsaini111 <youremail@example.com>
2025-11-24 21:06:40 +0530

    third time 4

--- src/app/legal-services/page.jsx
+++ src/app/legal-services/page.jsx
@@ -1,25 +1,28 @@
 "use client";
 
-import FaqSection from '@/components/FaqSection'
-import PrimaryServices from '@/components/PrimaryServices'
-import React from 'react'
-import GetInTouch from '@/components/GetInTouch';
-import Data from '@/Data/data.json'
-  const faqs = Data.primaryServices.faqs;
-function page() {
-  return (
-   <>
-    <PrimaryServices/>
-    <FaqSection faqs={faqs} />
-     <GetInTouch
-      variant="blue"
-  height="470px"
-  title="Need guidance?"
-  subtitle="If you're dealing with an issue related to this service, feel free to reach out. We'll explain your options clearly and guide you through the right next steps."
-      />
- 
-   </>
-  )
-}
+import { Suspense } from "react";
+import FaqSection from "@/components/FaqSection";
+import PrimaryServices from "@/components/PrimaryServices";
+import GetInTouch from "@/components/GetInTouch";
+import Data from "@/Data/data.json";
 
-export default page
+const faqs = Data.primaryServices.faqs;
+
+export default function Page() {
+  return (
+    <>
+      <Suspense fallback={<div>Loading services...</div>}>
+        <PrimaryServices />
+      </Suspense>
+
+      <FaqSection faqs={faqs} />
+
+      <GetInTouch
+        variant="blue"
+        height="470px"
+        title="Need guidance?"
+        subtitle="If you're dealing with an issue related to this service, feel free to reach out. We'll explain your options clearly and guide you through the right next steps."
+      />
+    </>
+  );
+}

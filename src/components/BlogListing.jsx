c42f75bb426a21b4f92d8f4ff8cbe45dc90da7fb
Ayush Saini <Oryviaprojects@gmail.com>
2025-11-27 17:53:51 +0530

    Fix: correct Git author

--- src/components/BlogListing.jsx
+++ src/components/BlogListing.jsx
@@ -26,7 +26,7 @@ export default function BlogVideoListingPage({ searchQuery = "", onClearSearch }
       }
 
       const queryParam = query ? `&query=${encodeURIComponent(query)}` : "";
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog?page=${pageNum}&limit=${limit}${queryParam}`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/blog?page=${pageNum}&limit=${limit}${queryParam}`);
       const json = await res.json();
 
       const blogs = json?.data?.blogs || [];
